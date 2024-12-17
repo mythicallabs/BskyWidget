34188dd4038886f64d0f456f04d56ae6fdbf51b5
Mythical Coding Labs <mythicallabsremote@gmail.com>
2024-12-17 08:19:14 -0600

    V1.0.0
    First release of BskyWidget

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// Line drawing function
+DrawContext.prototype.drawLine = function (p_x1, p_y1, p_x2, p_y2, p_colourLine, p_intWidth)
+{	
+	let pthLine = new Path();
+	pthLine.move(new Point(p_x1, p_y1));
+	pthLine.addLine(new Point(p_x2, p_y2));
+	this.addPath(pthLine);
+	this.setStrokeColor(p_colourLine);	
+	this.setLineWidth(p_intWidth);
+	this.strokePath();
+}
 var widget = new ListWidget()
 if(args.widgetParameter){
   var request = new Request('https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=' + args.widgetParameter)
@@ -11,7 +22,7 @@ if(!username || username == ""){
 }
 var followCount = profile.followersCount
 text = widget.addText(username)
-text.font = new Font("seirf", 20)
+text.font = new Font("seirf", 24)
 if(followCount < 999){
   
 }else{
@@ -25,7 +36,18 @@ if(followCount < 999){
     }
   }
 }
-widget.addText(followCount.toString())
+let dcRegion = new DrawContext();
+dcRegion.size = new Size(282, 282);
+dcRegion.opaque = false;
+
+dcRegion.drawLine(20, 145, 170, 145, Color.gray(), 1);
+
+// Render the widget with the lines on it
+widget.backgroundImage = dcRegion.getImage();
+text = widget.addText(followCount.toString() + " followers")
+text.font = new Font("seirf", 14)
+text = widget.addText(profile.postsCount.toString() + " posts")
+text.font = new Font("seirf", 14)
 if (!config.runsInWidget) {
   await widget.presentSmall()
 }
